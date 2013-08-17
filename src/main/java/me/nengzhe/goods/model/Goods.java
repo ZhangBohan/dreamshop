a3b6b501a456706989a04614fc5f8094e14462b6
ZhangBohan <imzhangbohan@gmail.com>
2013-08-17 13:32:28 +0800

    add sign

--- src/main/java/me/nengzhe/goods/model/Goods.java
+++ src/main/java/me/nengzhe/goods/model/Goods.java
@@ -1,5 +1,7 @@
 package me.nengzhe.goods.model;
 
+import java.util.Date;
+
 /**
  * 商品实体
  *
@@ -14,6 +16,10 @@ public class Goods {
     private String cost;    // 成本
     private String specification;   // 规格
     private String unit;    // 单位
+
+    private Boolean delete; // 是否被删除
+    private Date modifiedAt; // 修改时间
+    private Date createAt; // 创建时间
 
     public Integer getId() {
         return id;
@@ -61,5 +67,29 @@ public class Goods {
 
     public void setUnit(String unit) {
         this.unit = unit;
+    }
+
+    public Boolean getDelete() {
+        return delete;
+    }
+
+    public void setDelete(Boolean delete) {
+        this.delete = delete;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Date modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
     }
 }
