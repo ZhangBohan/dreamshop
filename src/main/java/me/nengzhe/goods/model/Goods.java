386c707ac39a7c5054eca6ae5379138f099dc8c8
ZhangBohan <imzhangbohan@gmail.com>
2013-08-17 14:45:13 +0800

    SQL

--- src/main/java/me/nengzhe/goods/model/Goods.java
+++ src/main/java/me/nengzhe/goods/model/Goods.java
@@ -1,5 +1,6 @@
 package me.nengzhe.goods.model;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 /**
@@ -12,8 +13,8 @@ import java.util.Date;
 public class Goods {
     private Integer id;
     private String barCode;  // 条码
-    private String price;   // 价格
-    private String cost;    // 成本
+    private BigDecimal price;   // 价格
+    private BigDecimal cost;    // 成本
     private String specification;   // 规格
     private String unit;    // 单位
 
@@ -21,75 +22,5 @@ public class Goods {
     private Date modifiedAt; // 修改时间
     private Date createAt; // 创建时间
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getBarCode() {
-        return barCode;
-    }
-
-    public void setBarCode(String barCode) {
-        this.barCode = barCode;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getCost() {
-        return cost;
-    }
-
-    public void setCost(String cost) {
-        this.cost = cost;
-    }
-
-    public String getSpecification() {
-        return specification;
-    }
-
-    public void setSpecification(String specification) {
-        this.specification = specification;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public Boolean getDelete() {
-        return delete;
-    }
-
-    public void setDelete(Boolean delete) {
-        this.delete = delete;
-    }
-
-    public Date getModifiedAt() {
-        return modifiedAt;
-    }
-
-    public void setModifiedAt(Date modifiedAt) {
-        this.modifiedAt = modifiedAt;
-    }
-
-    public Date getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
 }
---
+++ src/main/resources/db/2013-08-17_02.sql
@@ -0,0 +1,12 @@
+CREATE TABLE goods(
+  id  INT PRIMARY KEY AUTO_INCREMENT,
+  bar_code VARCHAR(50) NOT NULL,
+  price DECIMAL NULL DEFAULT 0.0,
+  cost DECIMAL NULL DEFAULT 0.0,
+  specification VARCHAR(100) NULL,
+  unit VARCHAR(50) NULL,
+  status BOOLEAN DEFAULT FALSE,
+  modified_at DATETIME,
+  create_at DATETIME
+);
+CREATE INDEX goods_bar_code ON goods(bar_code(13));
